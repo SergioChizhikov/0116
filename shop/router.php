1bc07b35f7ac03fbbd1801f7789a4664cfa92be3
Chizhikov <57985484+SergioChizhikov@users.noreply.github.com>
2024-06-13 11:54:21 +0300

    registration work

--- shop/router.php
+++ shop/router.php
@@ -2,7 +2,8 @@
 
 
 $url = explode("/", $_SERVER["REQUEST_URI"]);
-
+require_once("php/db.php");
+require_once("php/classes/User.php");
 
 if ($url[1] == "login") {
   $content = file_get_contents("pages/login.php");
@@ -12,6 +13,10 @@ if ($url[1] == "login") {
   $content = file_get_contents("pages/contact.html");
 } else if ($url[1] == "users") {
   require_once("pages/users/index.html");
+} else if ($url[1] == "addUser") {
+  echo User::addUser($_POST["name"], $_POST["lastname"], $_POST["email"], $_POST["pass"]);
+} else if ($url[1] == "authUser") {
+  echo User::authUser($_POST["email"], $_POST["pass"]);
 } else {
   $content = file_get_contents("pages/index.php");
 }
